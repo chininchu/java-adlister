3d76460b8d0803b909729a96608190f699fc2886
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-16 13:32:21 -0500

    First Commit

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,8 +1,15 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+
 public class HelloWorldServlet extends HttpServlet {
+
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
     }
