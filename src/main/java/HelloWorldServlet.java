df9fa98070b59fa7aa4f61d8340454ce651e0a58
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-17 09:26:46 -0500

    SerLets - Final Commit

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,16 +1,44 @@
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/count")
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 
 public class HelloWorldServlet extends HttpServlet {
 
 
+    //    Finish up the Hello World Servlet
+//
+//    Create a page that displays "Hello World!" when a user visits /hello
+
+    private int count = 0;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, World!</h1>");
+
+        //    Create a page view counter
+        //    Create a page that displays a number that goes up by one every time the /count page is viewed.
+
+        count++;
+
+        String reset = request.getParameter("reset");
+
+        if (reset != null) {
+            count = 0;
+        }
+
+        out.println("<h1>" + count + "</h1>");
+
 
     }
+
+
 }
+
+
