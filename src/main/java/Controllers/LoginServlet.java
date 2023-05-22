d4ad2cf1305807058487aefd27e700e300372c3b
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-21 20:40:10 -0500

    Login Servlet and jsp file created

---
+++ src/main/java/Controllers/LoginServlet.java
@@ -0,0 +1,29 @@
+package Controllers;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Renders the login page and forwards data
+
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+
+
+    }
+}
+
+
+
+
+
