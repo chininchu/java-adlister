20077a3fdc129d14e9f36c976b26bdcf9c3c5a0a
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-25 14:38:02 -0500

    Final Commit - User Input

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -37,29 +37,33 @@ public class MySQLAdsDao implements Ads {
             return createAdsFromResults(rs);
 
         } catch (SQLException e) {
-            throw new RuntimeException("Error retrieving all ads.", e);
+            throw new RuntimeException("Error retrieving all ads.");
         }
     }
 
     @Override
     public Long insert(Ad ad) {
         try {
-            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+//            String createInsertQuery = "INSERT INTO ads(user_id, title, description) VALUES (?,?,?)";
+
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(), Statement.RETURN_GENERATED_KEYS);
 
             stmt.setLong(1, ad.getUserId());
             stmt.setString(2, ad.getTitle());
             stmt.setString(3, ad.getDescription());
 
+            stmt.executeUpdate();
+
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error creating a new ad.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating a new ad.");
         }
 
     }
 
-    private String createInsertQuery(Ad ad) {
+    private String createInsertQuery() {
         return "INSERT INTO ads(user_id, title, description) VALUES (?,?,?)";
 
 
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -2,15 +2,15 @@
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Your Profile" />
+        <jsp:param name="title" value="Your Profile"/>
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="/WEB-INF/partials/navbar.jsp" />
+<jsp:include page="/WEB-INF/partials/navbar.jsp"/>
 
-    <div class="container">
-        <h1>Welcome, ${sessionScope.user}!</h1>
-    </div>
+<div class="container">
+    <h1>Welcome, ${sessionScope.user}!</h1>
+</div>
 
 </body>
 </html>
