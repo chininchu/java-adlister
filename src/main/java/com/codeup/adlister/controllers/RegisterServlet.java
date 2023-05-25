54cc9f48fbebe37fbb8f503b420937c7e4bb70ce
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-24 20:26:56 -0500

    The registeration form is able to register the user and inserts a record in the database

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,70 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.Users;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        // TODO: show the registration form
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  {
+//        // TODO: show the registration form
+//
+//    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/register.jsp").forward(req, resp);
+
+
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        // TODO: ensure the submitted information is valid
-        // TODO: create a new user based off of the submitted information
-        // TODO: if a user was successfully created, send them to their profile
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//        // TODO: ensure the submitted information is valid
+//        // TODO: create a new user based off of the submitted information
+//        // TODO: if a user was successfully created, send them to their profile
+//    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        String email = req.getParameter("email");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+
+        if (username != null && password != null && email != null) {
+
+            User newUser = new User();
+
+            newUser.setEmail(email);
+            newUser.setPassword(password);
+            newUser.setUsername(username);
+
+
+            Users userDao = DaoFactory.getUsersDao();
+
+            userDao.insert(newUser);
+
+            resp.sendRedirect("/login");
+
+
+        } else {
+
+
+            resp.sendRedirect("/register");
+        }
+
+
     }
 }
