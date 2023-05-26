f569d1bc7285870a8568451a5b7e9e8f89b85428
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-26 17:32:49 -0500

    In the process of creating logic so only the user can create ads

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -13,15 +13,27 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+
+
+
+//        if (request.getSession().getAttribute("users") == DaoFactory.getUsersDao().findByUsername() ) {
+//
+//
+//            request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+//
+//            return;
+//
+//
+//        }
+
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description")
+                1, // for now we'll hardcode the user id
+                request.getParameter("title"),
+                request.getParameter("description")
         );
         DaoFactory.getAdsDao().insert(ad);
         response.sendRedirect("/ads");
